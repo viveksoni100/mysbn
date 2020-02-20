ed86ebbd2366b356aba95687a0fc81ff9b6e4e72
vivek.barbhaya <vivek.barbhaya@yahoo.com>
2020-02-21 01:10:35 +0530

    phpmailer is implemented successfully...

---
+++ Mail_Attemps/Version4/mail.php
@@ -0,0 +1,35 @@
+<?php
+// Load Composer's autoloader
+require 'PHPMailerAutoload.php';
+
+// Instantiation and passing `true` enables exceptions
+$mail = new PHPMailer;
+
+try {
+    //Server settings
+    $mail->SMTPDebug = 5;                      // Enable verbose debug output
+    $mail->isSMTP();                                            // Send using SMTP
+    $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+    $mail->Username   = 'ghanshyampande100@gmail.com';                     // SMTP username
+    $mail->Password   = 'Vivekbarbhaya@123';                               // SMTP password
+    $mail->SMTPSecure = 'ssl';         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` also accepted
+    $mail->Port       = 465;                                    // TCP port to connect to
+
+    //Recipients
+    $mail->setFrom('ghanshyampande100@gmail.com', 'Mailer');
+    $mail->addAddress('viveksoni100@gmail.com', 'Joe User');     // Add a recipient
+    // Content
+    $mail->isHTML(true);                                  // Set email format to HTML
+    $mail->Subject = 'Here is the subject';
+    $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+    if($mail->send())
+      echo 'Message has been sent';
+    else {
+      echo 'Work harder...';
+    }
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
